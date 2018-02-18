fb45cbf840da11a8fe16d18a54172f6af0ff9237
aaronsalva27 <aaronsalva27@gmail.com>
2018-02-18 18:07:45 +0100

    first commit

---
+++ app/src/main/java/edu/fje/dam/simon/MainActivity.java
@@ -0,0 +1,41 @@
+package edu.fje.dam.simon;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+    // Set the duration of the splash screen
+    private static final long SPLASH_SCREEN_DELAY = 3000;
+    public static final String EXTRA_MISSATGE = "edu.fje.dam2.data";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                goSimonActivity();
+            }
+        }, SPLASH_SCREEN_DELAY);
+
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * realizamos un intent y passamos el nombre del jugador
+     */
+    public void goSimonActivity(){
+        Intent intent = new Intent(this, SimonActivity.class);
+        intent.putExtra(EXTRA_MISSATGE, "" );
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/edu/fje/dam/simon/SimonActivity.java
@@ -0,0 +1,53 @@
+package edu.fje.dam.simon;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SimonActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MISSATGE = "edu.fje.dam2.data";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simon);
+    }
+
+
+    /**
+     * recuperamos el menu
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    /**
+     * funcion que se utiliza para gestionar las opciones del menú.
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+           default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    /**
+     * realizamos un intent y passamos el nombre del jugador
+     */
+    public void goEndActivity(){
+        Intent intent = new Intent(this, EndActivity.class);
+        intent.putExtra(EXTRA_MISSATGE, "" );
+        startActivity(intent);
+    }
+}
