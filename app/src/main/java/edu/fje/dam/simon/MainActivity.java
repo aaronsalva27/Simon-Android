ccb582d2b695e0aa5f47225a447c75347fc0303f
AlexArandaSalinas <aranda711@gmail.com>
2018-02-19 17:26:02 +0100

    Theme personalitzat y paantalla welcome

--- app/src/main/java/edu/fje/dam/simon/MainActivity.java
+++ app/src/main/java/edu/fje/dam/simon/MainActivity.java
@@ -23,7 +23,8 @@ public class MainActivity extends AppCompatActivity {
         new Timer().schedule(new TimerTask() {
             @Override
             public void run() {
-                goSimonActivity();
+                //goSimonActivity();
+                goWelcomeActivity();
             }
         }, SPLASH_SCREEN_DELAY);
 
@@ -35,6 +36,12 @@ public class MainActivity extends AppCompatActivity {
      */
     public void goSimonActivity(){
         Intent intent = new Intent(this, SimonActivity.class);
+        intent.putExtra(EXTRA_MISSATGE, "" );
+        startActivity(intent);
+    }
+
+    public void goWelcomeActivity(){
+        Intent intent = new Intent(this, WelcomeActivity.class);
         intent.putExtra(EXTRA_MISSATGE, "" );
         startActivity(intent);
     }
---
+++ app/src/main/java/edu/fje/dam/simon/WelcomeActivity.java
@@ -0,0 +1,70 @@
+package edu.fje.dam.simon;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class WelcomeActivity extends AppCompatActivity {
+    //button para iniciar partida
+    protected Button PlayButton;
+    // nombre del jugador
+    public static final String EXTRA_MISSATGE = "edu.fje.dam2.data";
+    protected EditText editTextNom;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+
+        PlayButton = (Button) findViewById(R.id.ButtonPlay);
+
+        // función anonima para cambiar de actividad
+        PlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Go to: SimonActivity");
+                sendMessage();
+            }
+        });
+    }
+
+    /**
+     * recuperamos el menu
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    /**
+     * funcion que se utiliza para gestionar las opciones del menú.
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    /**
+     * realizamos un intent y passamos el nombre del jugador
+     */
+    public void sendMessage(){
+        Intent intent = new Intent(this, SimonActivity.class);
+        //intent.putExtra(EXTRA_MISSATGE, editTextNom.getText().toString());
+        startActivity(intent);
+    }
+}
