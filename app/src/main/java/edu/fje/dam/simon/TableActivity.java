151211943249d2eb9cacbbe224bfacccd529a431
aaronsalva27 <aaronsalva27@gmail.com>
2018-02-25 22:47:21 +0100

    logic v1

--- app/src/main/java/edu/fje/dam/simon/TableActivity.java
+++ app/src/main/java/edu/fje/dam/simon/TableActivity.java
@@ -3,16 +3,23 @@ package edu.fje.dam.simon;
 import android.content.Context;
 import android.content.Intent;
 import android.media.Image;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 import edu.fje.dam.simon.Adapters.ImageAdapter;
 import edu.fje.dam.simon.Services.AudioIntentService;
@@ -25,9 +32,17 @@ public class TableActivity extends AppCompatActivity {
     private boolean isReproduint= true;
     ArrayAdapter<Image> Adapter;
     GridView tableGrid;
-    public Context context;
+    private Context context;
+    private ImageView randomImage;
+    Random r = new Random();
 
-    int images[] = {
+
+    private int randomImageSelected;
+    private ArrayList<Integer> lastImages = new ArrayList<>();
+    private ArrayList<Integer> imagesSelected = new ArrayList<>();
+    private int turno = 0;
+
+    private int images[] = {
             R.drawable.quadrado_red,
             R.drawable.quadrado_blue,
             R.drawable.quadrado_green,
@@ -44,6 +59,8 @@ public class TableActivity extends AppCompatActivity {
             R.drawable.rombo_yellow
     };
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,8 +74,15 @@ public class TableActivity extends AppCompatActivity {
         context = getApplicationContext();
         tableGrid = (GridView) findViewById(R.id.table);
 
+
+        randomImage = (ImageView) findViewById(R.id.randomImage);
+
+
+        changeImage();
+
+        fadeImage(3000,1,false);
+
         tableGrid.setAdapter(new ImageAdapter(context,images));
-        //figuresGrid.setAdapter(Adapter);
 
         tableGrid.setOnItemClickListener(new AdapterView.OnItemClickListener()
         {
@@ -67,8 +91,76 @@ public class TableActivity extends AppCompatActivity {
             {
                 Toast.makeText( context,"pic" + (position + 1) + " selected",
                         Toast.LENGTH_SHORT).show();
+
+                imagesSelected.add(position);
+                checkResponse();
+
+
             }
         });
+
+    }
+
+    private void checkResponse() {
+
+        if(imagesSelected.get(turno) == lastImages.get(turno)) {
+            Log.d("SAVA", "OK");
+            turno++;
+            showResponses();
+        }else {
+            Log.d("SAVA", "MAL");
+        }
+
+    }
+
+    private void showResponses() {
+        Log.d("SAVA", "SHOW RESPONSES");
+        Log.d("SAVA", imagesSelected.toString());
+        Log.d("SAVA", ""+imagesSelected.size());
+        for (int i =0; i < imagesSelected.size(); i++) {
+            final int finalI = i;
+            Log.d("SAVA", ""+imagesSelected.size());
+            randomImage.animate().setDuration(2000)
+                    .withStartAction(new Runnable() {
+                        @Override
+                        public void run() {
+                            Log.d("SAVA", "SHOW RESPONSES"+finalI);
+                            randomImage.setImageResource(images[finalI]);
+                        }
+                    });
+
+        }
+
+        randomImage.setAlpha(0.0f);
+        fadeImage(3000,1,true);
+    }
+
+    private void fadeImage(int duration, int alpha, final boolean callback) {
+        randomImage.animate().setDuration(duration).alpha(alpha)
+            .withStartAction(new Runnable() {
+                @Override
+                public void run() {
+                    if(callback) {
+                        changeImage();
+                    }
+                }
+            })
+
+            .withEndAction(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+    }
+
+    private void changeImage() {
+        randomImageSelected = r.nextInt(images.length);
+        Log.d("SAVA", "New image "+randomImageSelected);
+        randomImage.setImageResource(images[randomImageSelected]);
+
+        lastImages.add(randomImageSelected);
+
     }
 
     /**
